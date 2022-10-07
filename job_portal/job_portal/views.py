12d206f21b078cf133c698be0190c4fd27f40e2f
noufida <noufidap@gmail.com>
2022-10-07 10:52:06 +0530

    index edited

--- job_portal/job_portal/views.py
+++ job_portal/job_portal/views.py
@@ -1,9 +1,6 @@
 
-
 from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
-from django.shortcuts import render,redirect
-from django.conf import settings
 
-def index(request):
-    return render(request,'index.html')
+
+index=never_cache(TemplateView.as_view(template_name='index.html'))
